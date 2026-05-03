cfe50ee527a05e7b058567ceaf601287e71a1d6c
Doc Walker <4-20ma@wvfans.net>
2026-05-02 20:49:30 -0500

    Update homepage and repository URLs for CryptoKitCLI

--- formula.rb
+++ formula.rb
@@ -1,9 +1,9 @@
 class CryptoKitcli < Formula
   desc "CryptoKitCLI"
-  homepage "https://github.com/alexruperez/CryptoKitCLI"
-  url "https://github.com/alexruperez/CryptoKitCLI/archive/0.1.0.tar.gz"
+  homepage "https://github.com/20ma/CryptoKitCLI"
+  url "https://github.com/20ma/CryptoKitCLI/archive/0.1.0.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  head "https://github.com/alexruperez/CryptoKitCLI.git"
+  head "https://github.com/4-20ma/CryptoKitCLI.git"
 
   depends_on xcode: ["11.4", :build]
 
